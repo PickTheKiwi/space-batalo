9ca31f0ad4f5ac0da1aa28d43ea8d1bd73310aa5
pickj888@gmail.com <pickj888@gmail.com>
2022-05-25 14:37:15 +1200

    Switched to 3d arrays

--- src/Main.java
+++ src/Main.java
@@ -11,22 +11,29 @@
 public class Main {
     private int x;
     private int y;
-
+    private int z;
+    final static int players = 2; // Never changes
     public static void main(String[] args) {
         // Create 2 arrays of size 10
-        int[][] playerGrid = new int[10][10]; // I do it like this because the IDE complains otherwise
-        int[][] botGrid = new int[10][10];
+        int[][][] grids = new int[10][10][players]; // I do it like this because the IDE complains otherwise
+        for (int z=0;z<players;z++) { // Loop through the z-axis
+            for (int y = 0; y < 10; y++) { // Loop through the y-axis
+                for (int x = 0; x < 10; x++) { // Loop through the x-axis
+                    grids[x][y][z] = 0; // Sets the cell in the grid to 0
+                }
+            }
+        }
 
-        for (int y=0;y<10;y++) { // Loop through the y-axis
-            for (int x=0;x<10;x++) { // Loop through the x-axis
-                playerGrid[x][y] = 0; // Sets the cell in the grid to 0
+        // Repeat the last loop
+        for (int z=0;z<players;z++) { // Loop through the z-axis
+            for (int y = 0; y < 10; y++) { // Loop through the y-axis
+                for (int x = 0; x < 10; x++) { // Loop through the x-axis
+                    System.out.print(grids[x][y][z] + " "); // Prints the cell in the grid
+                }
+                System.out.println(); // Prints a new line
             }
+            System.out.println(); // Prints a new line
         }
-        System.out.println(); // Creating gap between grids.
-        for (int y=0;y<10;y++) { // Loop through the y-axis
-            for (int x=0;x<10;x++) { // Loop through the x-axis
-                botGrid[x][y] = 0; // Sets the cell in the grid to 0
-            }
-        }
+
     }
 }
