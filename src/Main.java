b4dc7988341b79dc2490988b3df40a48b8a4ae43
pickj888@gmail.com <pickj888@gmail.com>
2022-06-05 17:38:03 +1200

    added player class and moved code over

--- src/Main.java
+++ src/Main.java
@@ -3,33 +3,22 @@
  * This class is responsible for initializing/running the game
  * and will be where most of the code is.
  *
- *
  * @author Seb White
- * @version 1
+ * @version 0.1
  */
-
 public class Main {
-    final static int players = 2; // Never changes
-    public static void main(String[] args) {
-        // Create 2 arrays of size 10
-        int[][][] grids = new int[10][10][players]; // I do it like this because the IDE complains otherwise
-        for (int z=0;z<players;z++) { // Loop through the z-axis
-            for (int y = 0; y < 10; y++) { // Loop through the y-axis
-                for (int x = 0; x < 10; x++) { // Loop through the x-axis
-                    grids[x][y][z] = 0; // Sets the cell in the grid to 0
-                }
-            }
-        }
+    final static int humanCount = 1; // Amount of humans in game, I chose to do this in case of more than one human playing
+    final static int botCount = 1; // Amount of bots in game, I chose to do this in case of more than one bot playing
+    final static int playerCount = humanCount + botCount; // Total players, including both humans and bots
 
-        // Repeat the last loop but print the grid
-        for (int z=0;z<players;z++) { // Loop through the z-axis
-            for (int y = 0; y < 10; y++) { // Loop through the y-axis
-                for (int x = 0; x < 10; x++) { // Loop through the x-axis
-                    System.out.print(grids[x][y][z] + " "); // Prints the cell in the grid
-                }
-                System.out.println(); // Prints a new line
-            }
-            System.out.println(); // Prints a new line
+    public int[][][] grids = new int[10][10][playerCount];
+
+    public static void main(String[] args) {
+        // load the Player class into as many objects as playerCount says
+        Player[] players = new Player[playerCount];
+        for (int x = 0; x < playerCount; x++) { // For as many players as there are
+            players[x] = new Player(true, playerCount); // Create a new player
         }
+        players[0].viewBoard(); // View the board
     }
 }
