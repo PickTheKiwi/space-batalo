d2d18b6a26353fe0adfa782e6e8eb2d3eebb3693
PickJ888 <PickJ888@gmail.com>
2022-06-30 11:09:15 +1200

    Edited names of final variables

--- src/Main.java
+++ src/Main.java
@@ -7,35 +7,50 @@
  * @version 0.1
  */
 import java.util.Scanner;
-import java.util.Random;
-import java.io.IOException;
 
 public class Main {
-    final static int humanCount = 1; // Amount of humans in game, I chose to do this in case of more than one human playing
-    final static int botCount = 1; // Amount of bots in game, I chose to do this in case of more than one bot playing
-    final static int playerCount = humanCount + botCount; // Total players, including both humans and bots
+    
+    final static int HUMANCOUNT = 1; // Amount of humans in game, I chose to do this in case of more than one human playing
+    final static int BOTCOUNT = 1; // Amount of bots in game, I chose to do this in case of more than one bot playing
+    final static int PLAYERCOUNT = HUMANCOUNT + BOTCOUNT; // Total players, including both humans and bots
 
-    final static int gridSize = 10; // Size of grid
+    final static int GRIDSIZE = 10; // Size of grid, do not set above 10, you will break the program due to how position input is taken.
 
+    public static int whichPlayer = 1;
+    public static Boolean gameRunning = true; // 
+    
     public static String alignment; // Where the alignment will be saved temporarily. Will only be used to set up the ships.
     public static String positions; // Where the positions will be saved temporarily
 
     public static void main(String[] args) {
-        // load the Player class into as many objects as playerCount says
-        Player[] players = new Player[playerCount];
-        for (int x = 0; x < humanCount; x++) { // For as many players as there are
-            players[x] = new Player(true, playerCount, gridSize); // Create a new player
 
-        }
-        for (int x = humanCount; x < playerCount; x++) { // For as many players as there are
-            players[x] = new Player(false, playerCount, gridSize); // Create a new player
-        }
-        for (int x = 0; x < playerCount; x++) { // For as many players as there are
-            players[x].viewBoard(); // View the board
+        if (args.length != 0) {
+            System.out.println("Arguments are not needed to run the game");
+            System.exit(2);
         }
 
+        // load the Player class into as many objects as PLAYERCOUNT is
+        Player[] players = new Player[PLAYERCOUNT];
+        for (int x = 0; x < HUMANCOUNT; x++) { // For as many players as there are
+            players[x] = new Player(true, PLAYERCOUNT, GRIDSIZE); // Create a new player
+
+        }
+        for (int x = HUMANCOUNT; x < PLAYERCOUNT; x++) { // For as many players as there are
+            players[x] = new Player(false, PLAYERCOUNT, GRIDSIZE); // Create a new player
+        }
+        for (int x = 0; x < PLAYERCOUNT; x++) { // For as many players as there are
+            players[x].viewBoard(); // View the board before game start - This is will be removed in the future
+        }
+
+        while (gameRunning) { // Because I want the game to end when player board is empty, not after all player have had their turn and a board is empty
+            if(whichPlayer < PLAYERCOUNT) {
+
+                whichPlayer++;
+            }
+            else {
+                whichPlayer = 0;
+            }
+        }
 
     }
-
-
 }
